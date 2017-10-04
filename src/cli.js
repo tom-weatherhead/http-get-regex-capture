5d2fa7e4a27d84cf83b99118d58289915327dc24
Tom Weatherhead <tom_weatherhead@hotmail.com>
2017-10-03 20:00:26 -0600

    Ready to release

--- src/cli.js
+++ src/cli.js
@@ -1,18 +1,32 @@
+#!/usr/bin/env node
+
 // http-get-regex-capture/src/cli.js
 
 'use strict';
 
 const matchRegexesInWebPage = require('..');
 
-function testHarness (url, regexes) {
+const targetName = process.argv.length > 2 && process.argv[2];
+
+function printTargetLatestVersion (url, regexes) {
 	matchRegexesInWebPage(url, regexes)
 		.then(result => {
-			console.log('Result:', result);
+			console.log(result.regexMatchResults[0].match);
 		}).fail(error => {
-			console.error('Error:', error);
+			console.error('printTargetLatestVersion: Error:', error);
 		})
 		.done();
 }
 
-testHarness('https://nodejs.org/en/', [/Download v(\S+)\s+Current/]);
-testHarness('https://www.ruby-lang.org/en/downloads/', [/The current stable version is (\S+)\.\s+Please/]);
+switch (targetName) {
+	case 'node':
+		printTargetLatestVersion('https://nodejs.org/en/', [/Download v(\S+)\s+Current/]);
+		break;
+
+	case 'ruby':
+		printTargetLatestVersion('https://www.ruby-lang.org/en/downloads/', [/The current stable version is (\S+)\.\s+Please/]);
+		break;
+
+	default:
+		console.error('Usage: thaw-latest-version-of [ node | ruby ]');
+}
